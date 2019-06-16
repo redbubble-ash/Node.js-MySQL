877378f5b431d2111f60ccba7677ef90c0c2b865
Your Name <you@example.com>
2019-06-16 16:34:25 -0700

    display products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
+
 
 
 var con = mysql.createConnection({
@@ -15,6 +17,12 @@ database: process.env.MYSQL_DATABASE_NAME
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    con.query("SELECT item_id, product_name, price FROM products", function(err, result, fields){
+    if (err) throw err;
+    console.log("Products are available to sale:\n"+cTable.getTable(result))
+
+    });
   });
+  
   
 //   con.end();
