4da4a93598acc4971ab3e8dc428d3e159201f628
Your Name <you@example.com>
2019-06-16 17:24:57 -0700

    ask product ID

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,13 +16,32 @@ database: process.env.MYSQL_DATABASE_NAME
 
 con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+    // console.log("Connected!");
+    // console.log("connected as id " + con.threadId + "\n");
     con.query("SELECT item_id, product_name, price FROM products", function(err, result, fields){
     if (err) throw err;
-    console.log("Products are available to sale:\n"+cTable.getTable(result))
+    console.log("\nProducts are available to sale:\n"+cTable.getTable(result))
 
     });
+    runSearch();
   });
+
+  function runSearch(){
+      inquirer
+      .prompt({
+          name: "productId",
+          type: "input",
+          message: "Which product would you like to buy? Please enter product ID."
+
+      })
+      .then(function(answer){
+          var query = "SELECT price, stock_quantity FROM products WHERE ?";
+          con.query(query, {item_id: answer.productId }, function(err, res){
+              if (err) throw err;
+              console.log(res[0].price);
+          })
+      })
+  }
   
   
 //   con.end();
