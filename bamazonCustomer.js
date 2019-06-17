924329f8beeb498b02dee08636659da67ba6f3df
Your Name <you@example.com>
2019-06-16 22:24:48 -0700

    ask user input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -43,52 +43,21 @@ con.connect(function(err) {
           }
       ];
 
-      inquirer.prompt(questions, function(answer){
-        console.log(answer);
+      inquirer.prompt(questions).then(function(answer){
+        // console.log(answer);
         var query = "SELECT price, stock_quantity FROM products WHERE ?";
         con.query(query, {item_id: answer.productId }, function(err, res){
             if (err) throw err;
             unitPrice = res[0].price;
+            // console.log(unitPrice);
+            units = parseFloat(answer.unitsToBuy);
+            totalAmt = parseFloat((unitPrice * units).toFixed(2));
+            console.log("Total buy "+ units +" units");
+            console.log(totalAmt);
+    
         });
-        units = answer.unitsToBuy;
-        totalAmt = unitPrice * units;
-        console.log(totalAmt);
       });
 
-
-
-
-
-
-    //   inquirer
-    //   .prompt(
-     
-    //       {
-    //         name: "productId",
-    //         type: "input",
-    //         message: "Which product would you like to buy? Please enter product ID."
-    //       },
-    //       {
-    //         name: "unitsToBuy",
-    //         type: "input",
-    //         message: "How many units would you like to buy?"
-    //       }
-      
-    //     )
-    //     .then(function(answer) {
-
-    //         console.log(answer);
-    //         var query = "SELECT price, stock_quantity FROM products WHERE ?";
-    //         con.query(query, {item_id: answer.productId }, function(err, res){
-    //             if (err) throw err;
-    //             unitPrice = res[0].price;
-    //         });
-    //         units = answer.unitsToBuy;
-    //         totalAmt = unitPrice * units;
-    //         console.log(totalAmt);
-
-
-    //     });
   };
 
   
