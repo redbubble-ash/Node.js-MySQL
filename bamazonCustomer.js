98854a216dbda39c9f1ec9c2f8b655aa19b9be72
Your Name <you@example.com>
2019-06-16 18:18:32 -0700

    ask for unit to buy

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,25 +23,74 @@ con.connect(function(err) {
     console.log("\nProducts are available to sale:\n"+cTable.getTable(result))
 
     });
-    runSearch();
-  });
+    idSearch();
+});
 
-  function runSearch(){
-      inquirer
-      .prompt({
-          name: "productId",
-          type: "input",
-          message: "Which product would you like to buy? Please enter product ID."
+  function idSearch(){
+      var totalAmt = 0;
+      var unitPrice = 0;
 
-      })
-      .then(function(answer){
-          var query = "SELECT price, stock_quantity FROM products WHERE ?";
-          con.query(query, {item_id: answer.productId }, function(err, res){
-              if (err) throw err;
-              console.log(res[0].price);
-          })
-      })
-  }
+      var questions = [
+        {
+            name: "productId",
+            type: "input",
+            message: "Which product would you like to buy? Please enter product ID."
+          },
+          {
+            name: "unitsToBuy",
+            type: "input",
+            message: "How many units would you like to buy?"
+          }
+      ];
+
+      inquirer.prompt(questions, function(answer){
+        console.log(answer);
+        var query = "SELECT price, stock_quantity FROM products WHERE ?";
+        con.query(query, {item_id: answer.productId }, function(err, res){
+            if (err) throw err;
+            unitPrice = res[0].price;
+        });
+        units = answer.unitsToBuy;
+        totalAmt = unitPrice * units;
+        console.log(totalAmt);
+      });
+
+
+
+
+
+
+    //   inquirer
+    //   .prompt(
+     
+    //       {
+    //         name: "productId",
+    //         type: "input",
+    //         message: "Which product would you like to buy? Please enter product ID."
+    //       },
+    //       {
+    //         name: "unitsToBuy",
+    //         type: "input",
+    //         message: "How many units would you like to buy?"
+    //       }
+      
+    //     )
+    //     .then(function(answer) {
+
+    //         console.log(answer);
+    //         var query = "SELECT price, stock_quantity FROM products WHERE ?";
+    //         con.query(query, {item_id: answer.productId }, function(err, res){
+    //             if (err) throw err;
+    //             unitPrice = res[0].price;
+    //         });
+    //         units = answer.unitsToBuy;
+    //         totalAmt = unitPrice * units;
+    //         console.log(totalAmt);
+
+
+    //     });
+  };
+
   
   
-//   con.end();
+  
